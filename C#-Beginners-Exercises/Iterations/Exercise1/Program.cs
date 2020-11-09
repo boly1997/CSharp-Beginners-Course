e0f73f187d7b09e897d3df6488f7c00c69d25a4c
sebastian <boly1997@hotmail.com>
2020-11-10 00:35:16 +0100

    Exercise 1
    Iterations

---
+++ C#-Beginners-Exercises/Iterations/Exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = 0;
+            for (var i=1; i<101; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    j++;
+                }
+            }
+            Console.WriteLine("The amount is:" + " " + j);
+        }
+
+    }
+}
