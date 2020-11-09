fd41fc1e49fc3ee0c07be3c1189b9101eb87ec54
boly1997 <63886579+boly1997@users.noreply.github.com>
2020-11-09 01:38:15 +0100

    Add files via upload

---
+++ Conditionals/Season.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conditionals
+{
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+}
