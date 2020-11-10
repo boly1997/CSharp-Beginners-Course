b510bb16951d60590d582a50ae6148f25a2e9607
sebastian <boly1997@hotmail.com>
2020-11-10 02:03:22 +0100

    Exercise 2

---
+++ C#-Beginners-Exercises/Iterations/Exercise 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Write a number or tab ok:");
+                var text = Console.ReadLine();
+                if (text == "ok")
+                {
+                    Console.WriteLine(number);
+                    break;
+                }
+                var newNumber = Int32.Parse(text);                
+                number = newNumber + number;
+
+            }
+
+        }
+    }
+}
