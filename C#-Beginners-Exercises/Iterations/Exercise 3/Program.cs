255eb8fb926a3ef73bfbfcbbd5848b750e88c478
sebastian <boly1997@hotmail.com>
2020-11-11 22:31:54 +0100

    Exercise 3 and gitignore of 2

---
+++ C#-Beginners-Exercises/Iterations/Exercise 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resultado = 1;
+            Console.WriteLine("Please enter the number for the factorial Operation:");
+            var factoralNumber = Int32.Parse(Console.ReadLine());
+            for (var i = factoralNumber; i > 1; i--)
+            {
+                resultado = resultado * i;
+
+            }
+                Console.WriteLine(factoralNumber + "! =" + resultado);
+
+               
+            
+        }
+    }
+}
