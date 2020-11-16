3876f87114edb3ee4a5e08788db199d989783cad
sebastian <boly1997@hotmail.com>
2020-11-16 01:32:26 +0100

    Exercise 3

---
+++ C#-Beginners-Exercises/Text/Exercise3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your time in this Format (e.g. 19:00);");
+            string time = Console.ReadLine(); // deberia de ser 00:20
+            if (time.Length != 5)
+            {
+                Console.WriteLine("Invalid Format or Hour");
+            }
+            else 
+            {
+                var hoursAndMinutes= time.Split(":"); // esto me regresa ["00","20"]
+                var hoursInt = new int[1] { Convert.ToInt32(hoursAndMinutes[0]) };
+                var minutesInt = new int[1] { Convert.ToInt32(hoursAndMinutes[1]) };
+
+                if (hoursInt[0] >= 00 || hoursInt[0] <= 23)
+                {
+                    if (minutesInt[0] >= 00 || minutesInt[0] <= 59)
+                    {
+                        Console.WriteLine("ALL ITS ALRIGHT");
+                    } else
+                        Console.WriteLine("Invalid input");
+                }
+            }
+        }
+    }
+}
