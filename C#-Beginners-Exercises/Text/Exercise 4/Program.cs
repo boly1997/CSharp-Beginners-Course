ac36f05ae67c5f7c530b722610b7304d4fa2b533
sebastian <boly1997@hotmail.com>
2020-11-17 02:54:04 +0100

    exercise 4

--- C#-Beginners-Exercises/Text/Exercise 4/Program.cs
+++ C#-Beginners-Exercises/Text/Exercise 4/Program.cs
@@ -4,30 +4,37 @@ namespace Exercise_4
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Enter some words separated by space (e.g Hola israel como estas");
             string words = Console.ReadLine();
             
             string wordsLowers = words.ToLower(); // ("hola israel como estas")
             string[] wordsWithoutSpace = wordsLowers.Split(' '); // ["hola", "israel"....]
-            int numWords = wordsWithoutSpace.Length;
 
-            for (int i = 0; i < numWords; i++)
+            var wordsWithoutSpaceUpper = firstCharToUpper(wordsWithoutSpace);
+
+            string pascalCase = wordsWithoutSpaceUpper[0];
+
+            for (int i = 1; i < wordsWithoutSpaceUpper.Length; i++)
             {
-                string currentWord = wordsWithoutSpace[i]; // "hola"
-                string firstChar = currentWord[0].ToString().ToUpper(); // "H"
-                wordsWithoutSpace[i] = firstChar+ currentWord.Substring(1);  // "H" + "ola" => "Hola"
-            } //["Hola", "Como" ...]
-
-            string pascalCase = wordsWithoutSpace[0];
-
-            for (int i = 1; i < numWords; i++)
-            {
-                pascalCase += wordsWithoutSpace[i]; 
+                pascalCase += wordsWithoutSpaceUpper[i]; 
             } // (HolaIsraelComoEstas)
 
             Console.WriteLine(pascalCase);
         }
+
+        public static string[] firstCharToUpper (string[] wordsWithoutSpace)
+        {
+            for (int i = 0; i < wordsWithoutSpace.Length; i++)
+            {
+                string currentWord = wordsWithoutSpace[i]; // "hola"
+                string firstChar = currentWord[0].ToString().ToUpper(); // "H"
+                wordsWithoutSpace[i] = firstChar + currentWord.Substring(1);  // "H" + "ola" => "Hola"
+            } //["Hola", "Como" ...]
+
+            return wordsWithoutSpace;
+        }    
+
     }
 }
