6007d71be1de847b357153bc0df71521cb1241f9
sebastian <boly1997@hotmail.com>
2020-11-17 03:08:06 +0100

    Exercise 5

--- C#-Beginners-Exercises/Text/Exercise 5/Program.cs
+++ C#-Beginners-Exercises/Text/Exercise 5/Program.cs
@@ -6,23 +6,31 @@ namespace Exercise_5
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Write an English Word");
             string englishWord = Console.ReadLine();
-            int count = englishWord.Length;
-            int j = 0;
             //var wordInList = new List <string>() { englishWord };
 
-            for (var i=0; i<count; i++)
+            int j = vocalCount(englishWord);
+          
+
+            Console.WriteLine("Your total of Vocals are: " + j);
+        }
+
+        public static int vocalCount(string englishWord)
+        {
+            int j = 0;
+            for (var i = 0; i < englishWord.Length; i++)
             {
                 if (englishWord[i] == 'a' || englishWord[i] == 'e' || englishWord[i] == 'i' || englishWord[i] == 'o' || englishWord[i] == 'u')
                 {
                     j++;
                 }
             }
-
-            Console.WriteLine("Your total of Vocals are: " + j);
+            return j;
         }
+
     }
+
 }
